b264ff31f1f40863a4235e33432f7345e922caf3
James Tlaloc <jamestlaloc.dev@gmail.com>
2026-02-22 03:59:47 -0800

    Optimize algorithm by using Set to check for duplicates, making the function O(n) instead of O(n^2).

--- js/script.js
+++ js/script.js
@@ -3,18 +3,20 @@
 // If every element is distinct, return false
 
 // Input:
-const numbers = [1,2,3,4];
+const numbers = [];
 
 // Output: true;
 
 const containsDuplicate = (nums) => {
+  const seen = new Set();
+
   for(let i = 0; i < nums.length; i++) {
-    for(let j = i + 1; j < nums.length; j++) {
-      if(nums[i] === nums[j]) {
-        return true;
-      } 
+    if(seen.has(nums[i])) {
+      return true;
     }
+    seen.add(nums[i])
   }
+
   return false;
 }
 
